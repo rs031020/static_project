84f2e88e343986e12508c1a8bf5916f8c708ece9
鈴木礼美 <rm.m031020@gmail.com>
2023-10-18 18:32:46 +0900

    java　基本文法課題　再提出

--- src/main/java/practice/PracticeSwitch.java
+++ src/main/java/practice/PracticeSwitch.java
@@ -1,42 +1,40 @@
 package practice;
 
 import java.util.Random;
+//import java.util.Random;
 
 public class PracticeSwitch {
 
 	public static void main(String[] args) {
 
-		int[] num = new int[10]; //10個の配列を定義
+		int num[] = new int[10];//10個の配列を定義
 		Random rnd = new Random(); //Randomクラスを呼び出し		
-
 		for (int i = 0; i < num.length; i++) {
-			num[i] = rnd.nextInt(4); //0〜4までを取得
-		}
-		
-		String jobArray[] = { //職業の配列を定義
-		"1:勇者",
-		"2:バトルマスター",
-		"3:パラディン",
-		"4:海賊",
-		"5:魔法剣士"
-		};
-		
-		for (int result : num) { //num配列10個を繰り返す
-			switch (result) { //resultの値が下記caseの場合それぞれの処理
-			case 0:
-				System.out.println(jobArray[0]);
-				break;
+			num[i] = rnd.nextInt(5);
+			int jobNum = num[i] + 1; //配列のインデックス番号が1〜5で表示されるようにする
+
+			String jobArray[] = { //職業の配列を定義
+					"勇者",
+					"バトルマスター",
+					"パラディン",
+					"海賊",
+					"魔法剣士"
+			};
+			switch (jobNum) { //resultの値が下記caseの場合それぞれの処理
 			case 1:
-				System.out.println(jobArray[1]);
+				System.out.println(jobNum + ":" + jobArray[0]);
 				break;
 			case 2:
-				System.out.println(jobArray[2]);
+				System.out.println(jobNum + ":" + jobArray[1]);
 				break;
 			case 3:
-				System.out.println(jobArray[3]);
+				System.out.println(jobNum + ":" + jobArray[2]);
 				break;
 			case 4:
-				System.out.println(jobArray[4]);
+				System.out.println(jobNum + ":" + jobArray[3]);
+				break;
+			case 5:
+				System.out.println(jobNum + ":" + jobArray[4]);
 				break;
 			}
 
--- src/main/java/practice/PracticeWhile.java
+++ src/main/java/practice/PracticeWhile.java
@@ -4,12 +4,11 @@ public class PracticeWhile {
 
 	public static void main(String[] args) {
 		int num = 100;
-		System.out.println(num);
 
-		while (num > 0) {
+		while (num >= 0) {
+			System.out.println(num);
 			num--;
 
-			System.out.println(num);
 		}
 	}
 }
